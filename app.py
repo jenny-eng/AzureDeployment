3c1ae2039332d7aba6f3600f4079404b9f276787
jenny-eng <103923008+jenny-eng@users.noreply.github.com>
2023-08-08 15:13:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,15 +18,15 @@ def predict():
     #int_features = [float(x) for x in request.form.values()]
     #int_features = [int(x) for x in request.form.values()]
     int_features = request.form.values().split(",")
-    final_features = [int(x) for x in int_features]
+    #final_features = [int(x) for x in int_features]
     #final_features = [np.array(int_features)]
     #prediction = model.predict(final_features)
-    prediction = np.sum(final_features)
+    #prediction = np.sum(final_features)
     #print(math.floor(prediction[0]))
 
     #output = round(prediction[0], 2)
     #return render_template('home.html', prediction_text="Predictd Truck Number is {}".format(math.floor(prediction[0])))
-    return render_template('home.html', prediction_text="Predictd Truck Number is {}".format(prediction))
+    return render_template('home.html', prediction_text="Predictd Truck Number is {}".format(int_features))
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
