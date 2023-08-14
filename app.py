3c65f466dff1820b0b32418e035fb6c30204d945
jenny-eng <103923008+jenny-eng@users.noreply.github.com>
2023-08-14 11:40:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,16 +29,16 @@ def predict():
     return render_template('home.html', prediction_text="Predictd Truck Number is {}".format(prediction[0])
 
                            
-#@app.route('/predict_api',methods=['POST'])
-#def predict_api():
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
     '''
     For direct API calls trought request
     '''
-#    data = request.get_json(force=True)
-#    prediction = model.predict([np.array(list(data.values()))])
-
-#    output = prediction[0]
-#    return jsonify(output)
+    data = request.get_json(force=True)
+    #prediction = model.predict([np.array(list(data.values()))])
+    prediction = [np.array(list(data.values()))]
+    #output = prediction[0]
+    return jsonify(prediction)
 
 
 
